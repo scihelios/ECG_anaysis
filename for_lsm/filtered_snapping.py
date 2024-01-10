255219faea65d58287c72efe678487637522bee1
Ahmed <ahmedmansourbourassine@gmail.com>
2024-01-10 21:06:04 +0100

    update

--- for_lsm/filtered_snapping.py
+++ for_lsm/filtered_snapping.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import numpy as np
+import random
 from scipy.signal import find_peaks
 import matplotlib.pyplot as plt
 
@@ -15,16 +16,8 @@ def load_signals(filename):
     filtered_signal = data[1::2]
     return raw_signal, filtered_signal
 
-def extract_cycles(signal):
-    # Détecter les pics R. Vous pourriez avoir besoin d'ajuster la distance et la hauteur selon la qualité de votre signal.
-    peaks, _ = find_peaks(signal, distance=50, height=70)
-    
-    # Découper le signal en cycles
-    cycles = [signal[peaks[i]:peaks[i+1]] for i in range(len(peaks)-1)]
-    
-    return cycles, peaks
-
 import json
+
 
 
 # Load the signals
@@ -37,28 +30,17 @@ def load_signals(filename):
     raw_signal = data[::2]
     filtered_signal = data[1::2]
 
-    return raw_signal, filtered_signal
+    return raw_signal/max(raw_signal), filtered_signal/max(filtered_signal)
 
 # Define the source directory where the 'Person_xx' folders are located.
 src_directory = Path("C:/Users/ahmed mansour/Desktop/scolarite X/2A/Psc/ECG_anaysis/collected data")
 count=0
-
+count_d = 0
 # Loop through each 'Person_xx' directory within the source directory.
 for signal in os.listdir(src_directory):
     # Charger le signal brut
-    raw_signal, _ = load_signals("C:/Users/ahmed mansour/Desktop/scolarite X/2A/Psc/ECG_anaysis/collected data/"+signal)
-    cycles, peaks = extract_cycles(raw_signal)
-
-    
-    for i in cycles:
-        ecg = i
-        test = np.concatenate((ecg[len(ecg)//2:len(ecg+1)],ecg[0:len(ecg)//2] ))
-        test = np.array(test)/100
-
-        with open("signal"+str(count)+".json", "w") as json_file:
-            json.dump({"signal":list(test) , "gaussienne":[]}, json_file)
-        count+=1
-        print(count)
-
-
-#raw_signal, filtered_signal = load_signals()
+    _, filtered_signal = load_signals("C:/Users/ahmed mansour/Desktop/scolarite X/2A/Psc/ECG_anaysis/collected data/"+signal)
+    with open("signal"+str(count)+".json", "w") as json_file:
+        json.dump({"signal":list(filtered_signal)}, json_file)
+    count+=1
+    print(count)
