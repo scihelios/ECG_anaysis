453a4f5c09297f341bcaec341eff0ce6d62294ce
Thomas B <thomas.boucas23@gmail.com>
2024-01-15 14:15:37 +0100

    Importation du code

---
+++ extremum.py
@@ -0,0 +1,149 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as sig
+import scipy.optimize as opt
+import imageio as io
+import copy
+import filter as flt
+import linsub as ls
+import time
+import parametres as par
+
+learning_rate = {
+    'Amplitude' : [3],
+    'Centre' : [0.0001],
+    'Ecart-type' : [0.001]
+}
+
+def ext(arr):
+    extrema_indices = []
+    extrema_values = []
+
+    for i in range(1, len(arr) - 1):
+        if arr[i-1] > arr[i] < arr[i+1] or arr[i-1] < arr[i] > arr[i+1]:
+            extrema_indices.append(i)
+            extrema_values.append(arr[i])
+
+    return extrema_indices, extrema_values
+
+
+def extremums(filt_beat, beat):
+    """
+    Fonction qui trouve et filtre les extrema d'un signal
+    """
+    n = len(filt_beat)
+    extrema_indices = []
+    extrema_values = []
+
+    for i in range(1, len(filt_beat) - 1):
+        if filt_beat[i-1] > filt_beat[i] < filt_beat[i+1] or filt_beat[i-1] < filt_beat[i] > filt_beat[i+1]:
+            extrema_indices.append(i)
+            extrema_values.append(beat[i])
+    x = extrema_indices
+    y = extrema_values
+
+    print(x)
+    print(y)
+
+    # # Filter for x values between 300 and 500
+    # filtered_indices = [i for i, val in enumerate(x) if 0.3 <= val/n <= 0.7]
+    # filtered_y = [y[i] for i in filtered_indices]
+
+    # if not filtered_indices:
+    #     return None, None
+
+    # # Find index of maximum y value in the filtered range
+    # max_y_index = filtered_indices[filtered_y.index(max(filtered_y))]
+
+    if not x or not y:
+        return None, None
+    
+    max_y_index = np.argmax(y)
+
+
+    # Indices around the max_y_index
+    indices = []
+    for i in range(-2,3,1):
+        if max_y_index + i < len(extrema_indices) and max_y_index + i >= 0:
+            indices.append(max_y_index + i)
+    new_x = []
+    new_y = []
+
+    for index in indices:
+        if 0 <= index < len(extrema_indices):  # Check if the index is within the list limits
+            new_x.append(x[index] / n * 2 * np.pi - np.pi)
+            new_y.append(y[index])
+
+    return new_x, new_y
+
+
+def init_parameters_extremums(nombre_gaussiennes, amplitudes,centres):
+    param = par.parametres()
+    param.nombre_gaussiennes = nombre_gaussiennes
+    param.amplitudes = amplitudes
+    param.centres = centres
+    param.ecarts_types = [0.05 for _ in range(nombre_gaussiennes)]
+    return param
+
+def loss_function(param, signal):
+    """
+    Fonction de coût
+    """
+    signal_gauss = param.signal_gaussiennes(len(signal))
+    return np.mean((signal - signal_gauss)**2)
+
+def gradient_descent(param, signal, eps = 0.01, itmax = 1000, learning_rate = learning_rate):
+    nombre_gaussiennes = param.nombre_gaussiennes # Assuming all lists in param have the same length
+
+    for iteration in range(itmax):
+        for i in range(nombre_gaussiennes):
+                
+            param.ecarts_types[i] += eps
+            loss_plus = loss_function(param, signal)
+
+            param.ecarts_types[i] -= 2*eps
+            loss_minus = loss_function(param, signal)
+
+            param.ecarts_types[i] += eps
+            grad = (loss_plus - loss_minus)/(2*eps)
+
+            param.ecarts_types[i] -= learning_rate['Ecart-type'][0] * grad 
+
+            param.centres[i] += eps
+            loss_plus = loss_function(param, signal)
+
+            param.centres[i] -= 2*eps
+            loss_minus = loss_function(param, signal)
+            
+            param.centres[i] += eps
+            grad = (loss_plus - loss_minus)/(2*eps)
+
+            param.centres[i] -= learning_rate['Centre'][0] * grad
+
+            param.amplitudes[i] += eps
+            loss_plus = loss_function(param, signal)
+
+            param.amplitudes[i] -= 2*eps
+            loss_minus = loss_function(param, signal)
+
+            param.amplitudes[i] += eps
+            grad = (loss_plus - loss_minus)/(2*eps)
+
+            param.amplitudes[i] -= learning_rate['Amplitude'][0] * grad
+    return param
+
+
+def gradient_descent_calibre(beat, learning_rate = learning_rate, pas = 10):
+    beat = ls.substract_linear(beat, pas)
+    filt_beat = beat.copy()
+    filt_beat = flt.lowpass_filter(filt_beat)
+    c,A = extremums(filt_beat, beat)
+    if c is None or A is None:
+        nombre_gaussiennes = 0
+    else: 
+        nombre_gaussiennes = len(c)
+    param = init_parameters_extremums(nombre_gaussiennes,A,c)
+    print(param)
+    return gradient_descent(param, beat, learning_rate=learning_rate), filt_beat
+
---
+++ testing_kalman.py
@@ -0,0 +1,101 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import time
+import parametres as par
+import linsub as ls
+import kalman as kalman
+
+#Simulation d'une gaussiennes bruitées
+
+input_folder = f'data/1/beats/1/1.npy'
+bit_resolution = 12
+max_range = 10 # mV
+yscale_factor = max_range / (2**(bit_resolution+1))
+
+
+#Paramètres des gaussiennes
+param = par.parametres()
+
+param.nombre_gaussiennes
+param.amplitudes = [50, -10, 120, -20, 50]
+param.centres = [-1.3, -0.4, 0, 0.2, 0.8]
+param.ecarts_types = [0.2, 0.1, 0.1, 0.1, 0.2]
+
+omega = 1
+#Abscisse du tracé
+val_gaussienne = np.load(input_folder)
+val_gaussienne = ls.substract_linear(val_gaussienne)
+n_point_theta = len(val_gaussienne)
+val_theta = np.linspace(-np.pi, np.pi, n_point_theta)
+dt = 2 * np.pi / omega * 1 / (n_point_theta + 1)
+
+
+
+#bruitage des données, creation de y
+
+y = np.array([val_gaussienne, val_theta])
+
+#Création des matrices de covariance
+
+#création de la matrice de mesure
+C = np.zeros((2, 18))
+C[0, 0] = 1
+C[1, 1] = 1
+
+#matrice de covariance du bruit
+
+niveau_bruit = np.var(val_gaussienne[0:50])
+niveau_bruit = 5
+P = niveau_bruit * np.eye(18) 
+Q = niveau_bruit/10000 * np.ones((18, 18))
+Q[0, 0] = niveau_bruit
+Q[1, 1] = niveau_bruit/10
+R = np.array([[niveau_bruit*50, 0], [0, 0]])
+
+
+#application du filtre de kalman
+
+#création de la donné initiale x0
+X = np.array([0, -np.pi, omega])
+
+x0 = kalman.formation(X, param)
+#nombre de fois que l'on va appliquer le filtre de Kalman sur un même signal
+nombre_periode = 40
+
+#on conserve toutes les iterations du filtre de kalman
+iteration_kalman = np.zeros((18, 1))
+
+for i in tqdm(range(nombre_periode)):
+        
+    #application du filtre
+
+    x_filtre = kalman.kalman(x0, y, P, Q, R, C, dt)
+
+    #on met à jour les paramètres initiaux
+    X, param = kalman.partitionnement(x_filtre[:,-1].reshape((18, 1)))
+
+    for j in range(5):
+        param.centres[j] = param.centres[j] % (2*np.pi) - np.pi
+
+    X[0] = 0
+    X[1] = -np.pi
+    X[2] = omega
+    x0 = kalman.formation(X, param)
+    
+    #on converse l'iteration de kalman dans le tableau iteration_kalman
+    iteration_kalman = np.hstack((iteration_kalman, x_filtre))
+    
+
+    
+iteration_kalman = iteration_kalman[:, 1:]
+
+X, param = kalman.partitionnement(iteration_kalman[:, -1].reshape((18, 1)))
+
+print(param)
+#param.plot_pics(yscale_factor)
+plt.plot(val_theta, yscale_factor *  y[0,:], label="signal bruité", alpha = 0.5)
+plt.plot(val_theta, yscale_factor * param.signal_gaussiennes(n_point_theta), label="signal filtré")
+plt.legend()
+plt.grid()
+plt.show()
